b5f5e97a753046d2280bdf08b0ef0b0c9cbd6017
David Martin <hwm@sfldesigners.com>
2016-10-06 10:15:02 -0400

    Niednagel quote

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -4,7 +4,8 @@ var quotes = [
   '“If you just work on stuff that you like and you’re passionate about, you don’t have to have a master plan with how things will play out.” <br/><b> - Mark Zuckerberg</b>',
   '"The best time to plant a tree was 20 years ago. The second best time is now." <br/><b> - Chinese proverb</b>',
   '“Never give in–never, never, never, never, in nothing great or small, large or petty, never give in except to convictions of honour and good sense. Never yield to force; never yield to the apparently overwhelming might of the enemy.” <br/><b> - Winston Churchill</b>',
-  '“If I have seen further than others, it is by standing on the shoulders of giants.” <br/><b> — Isaac Newton</b>'
+  '“If I have seen further than others, it is by standing on the shoulders of giants.” <br/><b> — Isaac Newton</b>',
+  '"Failure isn’t failure unless you don’t learn from it." <br/><b> - Dr. Ronald Niednagel'
 ]
 
 function newQuote() {
