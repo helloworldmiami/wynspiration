d3da4edfbcde315424e62981caf0e261e0e2caf0
nsellek <nick@seansellek.com>
2016-10-14 19:22:23 -0400

    Added service to grab contributers from the github api and display them to the main page

---
+++ app/services/contributor_finder_service.rb
@@ -0,0 +1,21 @@
+require 'octokit'
+require 'httparty'
+
+class ContributorFinderService
+  def self.find_contributors
+
+    client = Octokit::Client.new(:access_token => "#{ENV["WYNSPR_KEY"]}")
+
+    user = client.user
+    user.login
+
+    gitcon = HTTParty.get("https://api.github.com/repos/helloworldmiami/wynspiration/contributors")
+
+    justdoit = []
+
+      gitcon.each do |con|
+        justdoit << {link: con["html_url"], name: con["login"]}
+      end
+      justdoit
+  end
+end
